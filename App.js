8d0a52c93d7cd3bc0edfab25ad24ff32b8de1403
Guilherme Chaves Volpato <volpato2103@gmail.com>
2022-10-23 21:34:32 -0300

    correcao

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Movimentacao from './src/Pages/Movimentacao';
 import Home from './src/Pages/Home';
 import Calculadora from './src/Pages/Calculadora';
-import AddTarefa from './src/Pages/AdicionarTarefa';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +17,6 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} options={{header: () => null}}/>
         <Stack.Screen name="Calculadora" component={Calculadora} options={{header: () => null}}/>
         <Stack.Screen name="Movimentacao" component={Movimentacao} options={{header: () => null}}/>
-        <Stack.Screen name="AddTarefa" component={AddTarefa} options={{header: () => null}}/>
         </Stack.Navigator>
     </NavigationContainer>
   );
